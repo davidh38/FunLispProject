72dc13788b1fcf1b16285709e194d42e01da8faa
lifeainteasy@web.de <lifeainteasy@web.de>
2020-11-13 00:25:11 +0100

    Api test

--- fack.el
+++ fack.el
@@ -1,1 +1,33 @@
 ;; Emacs
+
+
+
+(require 'request)
+
+(defun test(a)
+(message "hi")
+(request
+ "https://api.covid19api.com/countries"
+ :sync t
+ :parser 'json-read
+ :success (cl-function
+           (lambda (&key data &allow-other-keys)
+             (insert "I sent: %S" (assoc-default 'args data))
+             (message "hello"))
+)
+:complete (cl-function
+           (lambda (&key response &allow-other-keys)
+             (message "Done: %s" (request-response-status-code response))))
+
+
+
+
+ )
+)
+
+
+(type-of (test 5))
+;;(message (request-response (test 5) 'status-code))
+
+;;(setq hello '((david . "5")(yes . "6")))
+;;(insert (cdr (assoc 'david hello)))
